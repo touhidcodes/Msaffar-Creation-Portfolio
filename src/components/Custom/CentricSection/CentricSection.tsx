97b18230c6e7b5a315792d8da67bb1c8f18d3f83
touhidcodes <touhidcodes@gmail.com>
2024-11-14 23:00:22 +0600

    fix: centric section design fixed

--- src/components/Custom/CentricSection/CentricSection.tsx
+++ src/components/Custom/CentricSection/CentricSection.tsx
@@ -7,19 +7,18 @@ const CentricSection = () => {
     <section className="max-w-screen-xl mx-auto lg:px-10">
       <div className="flex flex-col md:flex-row items-center justify-center md:justify-start p-8 md:p-16">
         {/* Left Side: Image */}
-        <div className="flex justify-center md:justify-start mb-8 md:mb-0 md:mr-8">
+        <div className="w-full lg:w-1/2">
           <Image
             src={centric}
             alt="User-centric approach"
-            width={200}
-            height={200}
-            className="w-40 h-40 md:w-64 md:h-64"
+            width={400}
+            height={400}
           />
         </div>
 
         {/* Right Side: Text content */}
-        <div className="text-center md:text-left max-w-lg">
-          <h2 className="text-3xl font-bold mb-4">
+        <div className="text-center md:text-left w-full lg:w-1/2">
+          <h2 className="text-4xl font-bold mb-4">
             Design Thinking: <br /> User-centric Approach
           </h2>
           <p className="text-gray-600 mb-6">
