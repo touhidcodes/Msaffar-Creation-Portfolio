6e3282ecdeb4ade229cd7cb5b4a639572c2c63ec
touhidcodes <touhidcodes@gmail.com>
2024-12-17 23:38:02 +0500

    x

--- src/components/Custom/ContactSection/ContactSection.tsx
+++ src/components/Custom/ContactSection/ContactSection.tsx
@@ -1,7 +1,7 @@
 const ContactSection = () => {
   return (
     <div>
-      <h2>Welcome to the ContactSection page added</h2>
+      <h2>Welcome to the ContactSection page</h2>
     </div>
   );
 };
