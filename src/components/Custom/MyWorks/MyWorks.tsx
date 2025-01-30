403a83a66f996346ef88a22844662173af2dad28
touhidcodes <touhidcodes@gmail.com>
2025-01-30 23:16:14 +0600

    add: data

--- src/components/Custom/MyWorks/MyWorks.tsx
+++ src/components/Custom/MyWorks/MyWorks.tsx
@@ -1,42 +1,5 @@
+import { demoWorkData } from "@/Data/Data";
 import React from "react";
-
-export const demoWorkData = [
-  {
-    id: 1,
-    title: "Flatmate Finder",
-    description: "A platform to find and connect with potential flatmates.",
-    technologies: ["Next.js", "TypeScript", "Redux"],
-    link: "#",
-  },
-  {
-    id: 2,
-    title: "Gift Shop Management",
-    description: "A dashboard for managing gifts and inventory.",
-    technologies: ["React.js", "MongoDB", "Tailwind CSS"],
-    link: "#",
-  },
-  {
-    id: 3,
-    title: "Summer Music Camp",
-    description: "A platform for music learning during summer vacations.",
-    technologies: ["React.js", "Firebase", "AOS"],
-    link: "#",
-  },
-  {
-    id: 4,
-    title: "Dashboard",
-    description: "A platform for music learning during summer vacations.",
-    technologies: ["React.js", "Firebase", "AOS"],
-    link: "#",
-  },
-  {
-    id: 5,
-    title: "Dashboard",
-    description: "A platform for music learning during summer vacations.",
-    technologies: ["React.js", "Firebase", "AOS"],
-    link: "#",
-  },
-];
 
 const MyWork = () => {
   return (
