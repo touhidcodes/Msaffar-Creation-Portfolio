2bd211e95acf28a1854d6eeeb812ece0ac216bbb
touhidcodes <touhidcodes@gmail.com>
2025-02-12 19:36:38 +0600

    add: centric section mobileresponsive

--- src/components/Custom/CentricSection/CentricSection.tsx
+++ src/components/Custom/CentricSection/CentricSection.tsx
@@ -4,29 +4,39 @@ import Button from "@/components/ui/Button/Button";
 
 const CentricSection = () => {
   return (
-    <section className="max-w-screen-xl mx-auto lg:px-10">
-      <div className="flex flex-col md:flex-row items-center justify-center md:justify-start p-8 md:p-16">
+    <section className="max-w-screen-xl mx-auto py-6 px-6 lg:py-10 lg:px-20">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-8">
         {/* Left Side: Image */}
-        <div className="w-full lg:w-1/2">
+        <div className="w-full lg:w-1/2 flex justify-center">
           <Image
             src={centric}
             alt="User-centric approach"
             width={400}
             height={400}
+            className="w-full max-w-sm lg:max-w-md h-auto"
           />
         </div>
 
-        {/* Right Side: Text content */}
-        <div className="text-center md:text-left w-full lg:w-1/2">
-          <h2 className="text-4xl font-bold mb-4">
-            Design Thinking: <br /> User-centric Approach
-          </h2>
-          <p className="text-gray-600 mb-6">
-            I specialize in creating user-centric solutions that align with
-            business objectives, drawing from a decade of industry expertise
-            across various sectors. Im currently based in Dubai!
-          </p>
-          <Button text="Let's Talk" link="/contact" />
+        {/* Right Side: Text content (Evenly spaced) */}
+        <div className="w-full lg:w-1/2 flex flex-col justify-between h-full text-center md:text-left">
+          <div>
+            <h2 className="text-4xl font-bold text-gray-800">
+              Design Thinking: <br />
+              User-centric Approach
+            </h2>
+            <p className="text-gray-600 mt-4">
+              I specialize in creating user-centric solutions that align with
+              business objectives, drawing from a decade of industry expertise
+              across various sectors. I'm currently based in Dubai!
+            </p>
+          </div>
+          <div className="mt-10">
+            <Button
+              text="Let's Talk"
+              link="/contact"
+              className="w-fit bg-black border-2 border-black text-white font-bold py-3 px-5 rounded-lg hover:border-2 hover:border-black hover:bg-white hover:text-black transition-all duration-300"
+            />
+          </div>
         </div>
       </div>
     </section>
