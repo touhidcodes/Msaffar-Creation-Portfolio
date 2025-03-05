9d3f2123703364ce43310f7588b5410c473f6848
touhidcodes <touhidcodes@gmail.com>
2025-03-05 23:32:40 +0600

    add: blog page

--- src/components/Custom/MyBlogs/MyBlogs.tsx
+++ src/components/Custom/MyBlogs/MyBlogs.tsx
@@ -1,9 +1,15 @@
-const MyBlogs = () => {
+import BlogCard from "@/components/BlogCard";
+import { blogs } from "@/data/blogs";
+
+export default function Home() {
   return (
-    <div>
-      <h2>Welcome to the MyBlogs page</h2>
+    <div className="max-w-3xl mx-auto p-5">
+      <h1 className="text-3xl font-bold mb-6 text-center">Latest Blogs</h1>
+      <div className="grid gap-6">
+        {blogs.map((blog) => (
+          <BlogCard key={blog.id} {...blog} />
+        ))}
+      </div>
     </div>
   );
-};
-
-export default MyBlogs;
+}
