8750fb8fae9370cf217cd23e3a7e3f099a5a94ba
touhidcodes <touhidcodes@gmail.com>
2025-03-18 22:37:24 +0600

    add: blog page content

--- src/components/Custom/MyBlogs/MyBlogs.tsx
+++ src/components/Custom/MyBlogs/MyBlogs.tsx
@@ -1,5 +1,9 @@
 const MyBlogs = () => {
-  return <section className="bg-gray-100"></section>;
+  return (
+    <section className="bg-gray-100">
+      <h1 className="text-4xl font-bold mb-6 text-center">My Featured Blog</h1>
+    </section>
+  );
 };
 
 export default MyBlogs;
