f918c0d01c23a6510480c94edd01ae933302014e
touhidcodes <touhidcodes@gmail.com>
2025-03-18 22:35:28 +0600

    del: blog page content

--- src/components/Custom/MyBlogs/MyBlogs.tsx
+++ src/components/Custom/MyBlogs/MyBlogs.tsx
@@ -1,14 +1,5 @@
 const MyBlogs = () => {
-  return (
-    <section className="bg-gray-100">
-      <div className="max-w-screen-xl mx-auto py-6 px-6 lg:py-10 lg:px-20">
-        <div className="text-center mb-6">
-          <h2 className="text-4xl font-bold text-gray-800">Featured Blogs</h2>
-          <p className="text-gray-600 mt-2">My Featured Blogs</p>
-        </div>
-      </div>
-    </section>
-  );
+  return <section className="bg-gray-100"></section>;
 };
 
 export default MyBlogs;
