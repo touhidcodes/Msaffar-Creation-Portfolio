d2b3ad040398938ea869ba2acc4103352134755c
touhidcodes <touhidcodes@gmail.com>
2025-05-20 23:04:38 +0600

    add: my works section button

--- src/components/Custom/MyWorks/MyWorks.tsx
+++ src/components/Custom/MyWorks/MyWorks.tsx
@@ -1,16 +1,16 @@
 "use client";
 
+import { Card } from "@/components/ui/card";
 import { recentWorks } from "@/data/Data";
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 import Image from "next/image";
-import { Card } from "@/components/ui/card";
-import { ArrowUpRight, Heart } from "lucide-react";
 import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+import "swiper/css";
+import "swiper/css/pagination";
+import Button from "@/components/ui/Button/Button";
 
 const MyWork = () => {
   return (
@@ -18,7 +18,7 @@ const MyWork = () => {
       {/* Hero Section */}
       <section className="text-center max-w-3xl mx-auto mb-12">
         <h1 className="text-4xl font-bold text-gray-900">
-          My Work: Highlighted Branding Projects
+          My Works: Highlighted Branding Projects
         </h1>
         <p className="text-gray-800 mt-4">
           Explore my top Logo & Branding projects highlighting innovative
@@ -55,9 +55,7 @@ const MyWork = () => {
                   </SwiperSlide>
                 ))}
               </Swiper>
-              <button className="absolute top-3 right-3 bg-white/70 rounded-full p-2">
-                <Heart className="w-5 h-5 text-gray-600" />
-              </button>
+              <button className="absolute top-3 right-3 bg-white/70 rounded-full p-2"></button>
             </div>
             <div className="flex justify-between items-center mt-4 px-1">
               <div>
@@ -84,6 +82,15 @@ const MyWork = () => {
             </div>
           </Card>
         ))}
+      </div>
+
+      {/* More Button */}
+      <div className="mt-10 mb-5 lg:mb-0 flex justify-center">
+        <Button
+          text="Explore More"
+          link="/project"
+          className="w-fit bg-black border-2 border-black text-white font-bold py-3 px-5 rounded-lg hover:border-2 hover:border-black hover:bg-white hover:text-black transition-all duration-300"
+        />
       </div>
     </div>
   );
