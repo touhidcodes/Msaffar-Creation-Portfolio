139cd24cb84e620dcee01ed1bff491f28e677fd9
touhidcodes <touhidcodes@gmail.com>
2025-06-06 19:59:53 +0600

    add: axios interceptor

---
+++ src/lib/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+// Add a request interceptor to include token
+axiosInstance.interceptors.request.use(
+  async function (config) {
+    const token = Cookies.get("token");
+    console.log(token);
+
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
