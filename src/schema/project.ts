690d1fc1fb080f93f2e868ea674ad63aac432254
touhidcodes <touhidcodes@gmail.com>
2025-06-06 20:00:25 +0600

    add: project schema

---
+++ src/schema/project.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const createProjectSchema = z.object({
+  name: z.string().min(1, "Project name is required"),
+  description: z.string().min(1, "Description is required"),
+  images: z.string().array().min(1, "At least one image URL is required"),
+  tags: z.string().array().min(1, "At least one tag is required"),
+  tools: z.string().array().min(1, "At least one tools name is required"),
+  isFeatured: z.boolean().default(false),
+  binanceProfileUrl: z.string().url().optional(),
+  client: z.string().optional(),
+  projectDuration: z.string().optional(),
+});
