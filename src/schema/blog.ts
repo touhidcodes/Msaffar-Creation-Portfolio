1963e9ea69d0c5e66230201ad18e4467b0c8b2ab
touhidcodes <touhidcodes@gmail.com>
2025-06-10 22:27:02 +0600

    add: blog zod schema

---
+++ src/schema/blog.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createBlogSchema = z.object({
+  title: z.string().min(1, "Project name is required"),
+  description: z.string().min(1, "Description is required"),
+  image: z.string().url("Valid image URL is required"),
+  isFeatured: z.boolean().default(false),
+  content: z.string().min(1, "Content URL is required"),
+});
